42f2abd5e6e9442845aee7958d2a2d9d6fb317b3
Ben Fields <me@benfields.net>
2013-04-18 10:48:02 +0100

    bypass and stopwords aren't beer, now ignored

--- newbeerin.py
+++ newbeerin.py
@@ -24,16 +24,20 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 from credentials import *
 
-TEMPLATES = ["New beer @{username}! Head there to find {beer}",
-             "Looking for {beer}? They just put it on @{username}",
-             "So @{username}, just put on {beer}, so you know"
+TEMPLATES = [u"New beer @{username}! Head there to find {beer}",
+             u"Looking for {beer}? They just put it on @{username}",
+             u"So @{username} just put on {beer}, so you know",
+             u"Thirsty? Grab some {beer} @{username}!"
              ]
 
-SHORT_TMPLS = ["New beers on klaxon, @{username}, too many to list, check: {link}",
-               "Lots of new beer on @{username}, {link}",
-               "So much new beer on @{username}, {link}",
-               "Looks like @{username} just put on a bunch of new beer {link}"
+SHORT_TMPLS = [u"New beers on klaxon, @{username}, too many to list, check: {link}",
+               u"Lots of new beer on @{username}, {link}",
+               u"So much new beer on @{username}, {link}",
+               u"Looks like @{username} just put on a bunch of new beer {link}",
+               u"Tasty new beer @{username}: {link}"
                ]
+
+BYPASS_WORDS = ['#otb', '#nowpouring', 'otb', 'on the bar', 'new on']
 
 def fetch_new_tweets(api, cursor = 0): 
     """grab new tweets from followers, since cursor, if given
@@ -54,7 +58,7 @@ def fetch_new_tweets(api, cursor = 0):
         yield tweet.user.screen_name, tweet.id, tweet.text
     
     
-def is_otb(model, tweet, bypass_words = ['#otb', '#nowpouring', 'otb', 'on the bar', 'new on']):
+def is_otb(model, tweet, bypass_words=BYPASS_WORDS):
     """run tweet on model to determine if it a probably OTB tweet returning true if it is
     if the tweet contains any bybass_words, does not run against model and returns true"""
     for word in bypass_words:
@@ -66,7 +70,7 @@ def is_otb(model, tweet, bypass_words = ['#otb', '#nowpouring', 'otb', 'on the b
     return False
 
     
-def split_beers(tweet, max_intro_prop=0.33):
+def split_beers(tweet, max_intro_prop=0.33, stops=BYPASS_WORDS):
     """break tweet into composite list of strings representing beers on offer
     currently uses"""
     tweet = tweet.lower()
@@ -93,6 +97,9 @@ def split_beers(tweet, max_intro_prop=0.33):
         tweet = pruned
         tweet = tweet.replace('keg', ' ')
     
+    for stop_phrase in stops:
+        tweet = tweet.replace(stop_phrase, ' ')
+    
     #split on newlines, then ',', then '.' use first that results in at least 3 tokens
     if len(tweet.split(','))>2:
         beers = tweet.split(',')
@@ -114,7 +121,8 @@ def split_beers(tweet, max_intro_prop=0.33):
     if len(beers)>0 and 'http' in beers[-1]:
         beers[-1] = beers[-1].split('http',1)[0]
     
-    return [b.strip().strip('.,') for b in beers if len(b)>0]
+    
+    return [b.strip().strip('.,?!:;').strip() for b in beers if len(b)>0 and not b.lower() in stops]
     
     
 def is_fresh(beer, days_old=90):
@@ -143,11 +151,15 @@ def tweet_these(api, beers, username, twid, dryrun=False, templates=TEMPLATES, s
        citing the name and id they came from if dryrun, 
        just print the tweet, else push it via api
        """
-    pretty_beer = reduce(lambda x,y:x.title()+', '+y, beers)
-    text = sample(templates, 1)[0].format(beer=pretty_beer.title(), username=username)
     bitly = bitly_api.Connection(access_token=BITLY_ACCESS_TOKEN)
     short = bitly.shorten('https://twitter.com/{user}/status/{twid}'.format(user=username, twid=twid))
-    text += ' '+ short[u'url']
+    if len(beers) > 0:
+        pretty_beer = reduce(lambda x,y:x.title()+u', '+y, beers).encode('utf8')
+        text = sample(templates, 1)[0].format(beer=pretty_beer.title(), username=username)
+        text += u' '+ short[u'url']
+    else:
+        print "No Beers, but tweeting a link for", twid
+        text = sample(short_tmpls,1)[0].format(username=username, link=short[u'url'])
     if len(text) > 140:
         text = sample(short_tmpls,1)[0].format(username=username, link=short[u'url'])
     if dryrun:
@@ -172,13 +184,16 @@ def main(argv=None):
         model = cPickle.load(rh)
     cursor = 0
     while True:
-        print "fetching new tweets…"
+        print time.asctime(), "fetching new tweets…"
         for username, twid, tweet in fetch_new_tweets(api, cursor):
             if username == the_tweeter:
                 #skip tweets from yourself.
                 continue
             if is_otb(model, tweet):
                 beers = split_beers(tweet)
+                if len(beers) == 0:
+                    #if we're here it means flagged as OTB but couldn't get any beer names out ot the tweet
+                     tweet_these(api, beers, username, twid, dryrun=dry_run)
                 new_beers = [beer for beer in beers if is_fresh(beer)]
                 if len(new_beers) > 0:
                     tweet_these(api, beers, username, twid, dryrun=dry_run)
